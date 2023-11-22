fb2540571a83ddbd2a181993077c1df473d50d9a
Alaa2472 <104609053+Alaa2472@users.noreply.github.com>
2023-11-22 21:19:54 +0000

    Version 0.0.1

---
+++ public/script.js
@@ -0,0 +1,11 @@
+
+
+
+    document.addEventListener("DOMContentLoaded", function () {
+      setTimeout(function () {
+        document.getElementById("loading-screen").style.display = "none";
+
+        document.getElementById("content").style.display = "block";
+      }, 2000); // 3000 milliseconds (3 seconds)
+    });
+
