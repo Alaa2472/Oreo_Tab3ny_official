ef74be7dd69ba8b20b2623f5a8e824ad8d4e5afd
Alaa2472 <104609053+Alaa2472@users.noreply.github.com>
2023-11-23 22:38:28 +0000

    رسؤ

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@
 
     document.addEventListener("DOMContentLoaded", function () {
       setTimeout(function () {
-        document.getElementById("loading-screen").style.display = "none";
+        document.getElementById("loader").style.display = "none";
 
         document.getElementById("content").style.display = "block";
       }, 2000); // 3000 milliseconds (3 seconds)
